84b0427696cacbb34fce369d2270fa5d23e7d978
Nina <Nina@localhost>
2017-06-04 16:30:57 +0200

    Klasse bereinigt

--- src/de/hdm/myjob/client/CreateBewerbung.java
+++ src/de/hdm/myjob/client/CreateBewerbung.java
@@ -1,7 +1,6 @@
 package de.hdm.myjob.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
 
 import de.hdm.myjob.shared.AdministrationAsync;
 import de.hdm.myjob.shared.bo.Benutzer;
